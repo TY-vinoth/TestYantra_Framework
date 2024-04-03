a5eccdc2af52cbd0f99c1f2c83c597150e731935
TY-vinoth <vinothkumar.e@testyantra.com>
2024-04-03 11:47:19 +0530

    jenkins push

--- src/main/java/webUtility/WebActions.java
+++ src/main/java/webUtility/WebActions.java
@@ -79,9 +79,9 @@ public class WebActions extends ReporterManager {
 		URL = "https://" + BSUserName + ":" + BSPassword + "@hub-cloud.browserstack.com/wd/hub";
 
 
-		/*execution_type = System.getProperty("execution_type","remote");
+		execution_type = System.getProperty("execution_type","remote");
 		browser = System.getProperty("browser_type","chrome");
-		platform = System.getProperty("platform_type","web");*/
+		platform = System.getProperty("platform_type","web");
 
 		switch (browser) {
 			case "chrome":
